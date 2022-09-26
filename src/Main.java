552a11b09d48474f22670c00a325eb0c48f34aec
Igor <99yearago99@gmail.com>
2022-09-26 17:42:20 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        int age = 7;
 
+        if(age>=18){
+            System.out.println("Ты совершеннолетний");
+        }
+        if(age<18);{
+            System.out.println("Ты несовершеннолетний");
+        }
     }
 }
