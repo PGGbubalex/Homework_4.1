2edbd70a10e6b52fb44c31ad28b9801294e941c0
Igor <99yearago99@gmail.com>
2022-09-26 18:11:20 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        int capicity = 102; // общая вместимость
+        int sits = 60; //  всего сидячих мест
+        int others = capicity - sits; // всего стоячих мест
+
+        int seating = 35; // занято сидячих мест
+        int stay = 25; // занято стоячих мест
+        int total = seating + stay;
+        System.out.println("Всего свободно " + (capicity - total) + " мест, из них " + (sits - seating) + " сидячих и " + (others - stay) + " стоячих");
+
 
     }
 }
