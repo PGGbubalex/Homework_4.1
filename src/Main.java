77dbb117dbb2ec25572cc717907a4b8cfd734fa0
Igor <99yearago99@gmail.com>
2022-09-26 17:58:38 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-
+    int age = 26;
+    if (age >=7 && age <18){
+        System.out.println("Ты ходишь в школу");
+    }
+    if (age >=18 && age<24){
+        System.out.println("Ты ходишь в университет");
+    }
+    if (age >=24){
+        System.out.println("Ты работаешь");
+    }
     }
 }
